aafc3174e008ad9736622a7d4b3631680d051651
RobinBroberg <robin.broberg86@gmail.com>
2023-03-24 19:03:44 +0100

    changes

--- startbootstrap-shop-homepage-gh-pages/js/scripts.js
+++ startbootstrap-shop-homepage-gh-pages/js/scripts.js
@@ -5,21 +5,33 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
 fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
   .then(data => {
-    const productContainer = document.getElementById('product-container');
+    const productContainer = document.querySelector('.row');
 
     data.forEach(product => {
       const productDiv = document.createElement('div');
-      productDiv.classList.add('product'); // add the "product" class
+      productDiv.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'mb-4');
       productDiv.innerHTML = `
-        <h3 class="title">${product.title}</h3>
-        <img class="images" src="${product.image}" alt="">
-        <p class="price">$${product.price}</p>
-        <p class="category">${product.category}</p>
+        <div class="card h-100">
+          <img class="card-img-top images mx-auto" src="${product.image}" alt="">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="card-title">${product.title}</h5>
+              <p class="card-text">$${product.price}</p>
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto mt-3" href="#">View options</a>
+            </div>
+          </div>
+        </div>
       `;
       productContainer.appendChild(productDiv);
     });
   })
   .catch(error => console.log(error));
+
